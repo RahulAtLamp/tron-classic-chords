ace499ba05c080090f6e9fe285d86a6b42c5845d
rahulatlamp <rahulrajan@lampros.tech>
2022-10-29 10:13:31 +0530

    Initial commit

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
 import "./navbar.scss";
 
 const Navbar = () => {
-  const [error, setError] = useState();
+  // const [error, setError] = useState();
 
   const [open, setOpen] = useState(false);
 
@@ -22,9 +22,9 @@ const Navbar = () => {
       <Link to="/" className="nav-logo">
         <img style={{ width: "250px", height: "80px" }} src={logo} />
       </Link>
-      <div onClick={handleClick} className="nav-icon">
+      {/* <div onClick={handleClick} className="nav-icon">
         {open ? <FiX /> : <FiMenu />}
-      </div>
+      </div> */}
       <ul className={open ? "nav-links active" : "nav-links"}>
         <li className="nav-item">
           <Link to="/" className="nav-link" onClick={closeMenu}>
