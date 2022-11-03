779cb6da488dc0b6adb8b1a7056f0fe01791f4c0
rahulatlamp <rahulrajan@lampros.tech>
2022-11-03 10:51:18 +0530

    FE Data

---
+++ src/components/explore/artist-dummy.js
@@ -0,0 +1,57 @@
+export const Artists = [
+    {
+        name: 'The Fray',
+        album_name: 'How to Save a Life',
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/1872f6e669484252bff3b720c04236de.jpg#1872f6e669484252bff3b720c04236de"
+    },
+    {
+        name: 'Arctic Monkeys',
+        album_name: 'The Car',
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/575fe58f686d1aba812ff5ff6ecef634.jpg#575fe58f686d1aba812ff5ff6ecef634"
+    },
+    {
+        name: 'Bright eyes',
+        album_name: 'Fevers and Mirrors',
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/2c7b4e08bf08486b96235bbf57cc9293.jpg#2c7b4e08bf08486b96235bbf57cc9293"
+    },
+    {
+        name: 'Damien Rice',
+        album_name: "B-Sides",
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/a88c44f50f2d4fe5cf49b463924c2929.jpg#a88c44f50f2d4fe5cf49b463924c2929"
+    },
+    {
+        name: "Jack's Mannequin",
+        album_name: "Everything in Transit",
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/300d94465f3348d2a32bddec29c5d916.jpg#300d94465f3348d2a32bddec29c5d916"
+    },
+    {
+        name: "Ben Folds",
+        album_name: "Rockin' The Suburbs",
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/59dafc15152c4214be9456eea63682e7.jpg#59dafc15152c4214be9456eea63682e7"
+    },
+    {
+        name: "Something Corporate",
+        album_name: "Leaving Through the Window",
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/8e0e5f9f0e194769ad72a23d5ba1bf70.jpg#8e0e5f9f0e194769ad72a23d5ba1bf70"
+    },
+    {
+        name: "Ben Folds Five",
+        album_name: "Ben Folds Five",
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/baec93c123794a0d9aa4c8fd103df3f4.jpg#baec93c123794a0d9aa4c8fd103df3f4"
+    },
+    {
+        name: "Keane",
+        album_name: "Hopes and Fears",
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/624a9f2e9695464c8cb4f898e7548c9f.jpg#624a9f2e9695464c8cb4f898e7548c9f"
+    },
+    {
+        name: "Augustana",
+        album_name: "Life Imitating Life",
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/a99c9cd5333740d88a58c03ec5f2343a.jpg#a99c9cd5333740d88a58c03ec5f2343a"
+    },
+    {
+        name: "Coldplay",
+        album_name: "Parachutes",
+        image: "https://lastfm.freetls.fastly.net/i/u/770x0/8df40383650e4c2ca26812f37c54d2bf.jpg#8df40383650e4c2ca26812f37c54d2bf"
+    }
+]
