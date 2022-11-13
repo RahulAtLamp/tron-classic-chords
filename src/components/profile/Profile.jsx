7a5d45cdcc46066075e2b65e4d6210a0dee7f5da
rahulatlamp <rahulrajan@lampros.tech>
2022-11-13 15:49:10 +0530

    Profile Page updated

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -15,6 +15,9 @@ const Profile = () => {
     const fileRef = useRef(null);
     const editUserPopup = useRef(null);
     const navigate = useNavigate();
+    const [userData, setUserData] = useState({ name: "", bio: "", profile_pic: null })
+    const [chain, setChainStatus] = useState(false);
+    const [profileImage, setprofileImage] = useState(null);
 
     const market_address = "0x0caC8C986452628Ed38483bcEE0D1cF85816946D";
     const classicChords_address = "0xed01Ed9D4dfa9BCb6540F71539c3D52EB3598212";
@@ -22,35 +25,82 @@ const Profile = () => {
     const firstFive = Collections.slice(0, 5);
     const lastFive = Collections.slice(-5);
 
-    const getProfile = async() => {
+    const getProfile = async () => {
         try {
             const { ethereum } = window;
             if (ethereum) {
-              const provider = new ethers.providers.Web3Provider(ethereum);
-              const signer = provider.getSigner();
-              if (!provider) {
-                console.log("Metamask is not installed, please install!");
-              }
-              const { chainId } = await provider.getNetwork();
-              console.log("switch case for this case is: " + chainId);
-              if (chainId === 1029) {
-                const contract = new ethers.Contract(market_address, market, signer);
-                const tx = await contract.userMapping(address);
-                console.log(tx);
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                if (!provider) {
+                    console.log("Metamask is not installed, please install!");
+                }
+                const { chainId } = await provider.getNetwork();
+                console.log("switch case for this case is: " + chainId);
+                if (chainId === 1029) {
+                    const contract = new ethers.Contract(market_address, market, signer);
+                    const tx = await contract.userMapping(address);
+                    console.log(tx);
                 } else {
-                alert("Please connect to the bitTorent Network!");
-              }
+                    // alert("Please connect to the bitTorent Network!");
+                    setChainStatus(true);
+                }
             }
-          } catch (error) {
+        } catch (error) {
             console.log(error);
-          }
-        }     
+        }
+    }
+
+    const addChain = () => {
+        if (window.ethereum) {
+            window.ethereum.request({
+                method: "wallet_addEthereumChain",
+                params: [{
+                    chainId: "0x405",
+                    rpcUrls: ["https://pre-rpc.bittorrentchain.io/"],
+                    chainName: "BitTorrent Chain Donau",
+                    // nativeCurrency: {
+                    //     name: "BitTorrent",
+                    //     symbol: "BTT",
+                    //     decimals: 18
+                    // },
+                    blockExplorerUrls: ["https://testscan.bittorrentchain.io/"]
+                }]
+            })
+        } else {
+            alert("Please Install a wallet to proceed.")
+        }
+    }
+
+    const checkChain = async () => {
+        if (window.ethereum) {
+            const { ethereum } = window;
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const { chainId } = await provider.getNetwork();
+            if (chainId !== 1029) {
+                setChainStatus(true);
+                return true;
+            } else {
+                setChainStatus(false);
+                return false;
+            }
+
+        } else {
+            alert("Please install a wallet.")
+        }
+    }
+
+    const addUserData = () => {
+        
+    }
 
 
     useEffect(() => {
         if (!isConnected) {
             navigate("/");
         }
+
+        checkChain();
+
         getProfile()
     }, []);
 
@@ -74,6 +124,18 @@ const Profile = () => {
         }
     }, [isConnected]);
 
+    useEffect(() => {
+        console.log(userData);
+    }, [userData])
+
+    useEffect(() => {
+        console.log(profileImage);
+        if (profileImage) {
+            const image = URL.createObjectURL(profileImage);
+            console.log(image);
+            setUserData({ ...userData, profile_pic: image });
+        }
+    }, [profileImage])
 
     return (
         <div className="profile-main">
@@ -93,7 +155,7 @@ const Profile = () => {
             <div className="profile-user-details">
                 <div className="profile-details-holder">
                     <h2 className="profile-username">Unknown</h2>
-                    <h3 className="profile-address">${address.slice(0, 4) + "...." + address.slice(-5)} </h3>
+                    <h3 className="profile-address">{address ? address.slice(0, 4) + "...." + address.slice(-5) : null} </h3>
                     <div className="profile-bio-holder">
                         <p className="profile-bio">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, molestias officiis non quod illum suscipit aspernatur fugit expedita, sunt eligendi, nisi dicta quae veniam consequuntur quos repellat molestiae recusandae explicabo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus assumenda ut et suscipit quaerat, nihil est deleniti enim officia modi itaque a atque ex doloribus aliquam reprehenderit saepe sequi placeat!
@@ -164,14 +226,28 @@ const Profile = () => {
                                 Edit Profile
                             </div>
                             <div className="profile-information">
-                                <input type="file" ref={fileRef} hidden />
+                                <input type="file" ref={fileRef} onChange={(e) => { setprofileImage(e.target.files[0]) }} hidden />
                                 <div className="update-profile-pic" onClick={() => { fileRef.current.click() }}>
-                                    <img src="images/man.png" alt="profile pic preview" className="profile-pic-preview" />
+                                    <img src={userData.profile_pic ? userData.profile_pic : "images/man.png"} alt="profile pic preview" className="profile-pic-preview" />
                                 </div>
-                                <input className="profile-username" type="text" placeholder="Username" />
-                                <textarea className="profile-bio" placeholder="Add your bio here..."></textarea>
-                                <button className="user-update-btn">Update Details</button>
+                                <input className="profile-username" type="text" placeholder="Username" onChange={(e) => { setUserData({ ...userData, name: e.target.value }) }} />
+                                <textarea className="profile-bio" placeholder="Add your bio here..." onChange={(e) => { setUserData({ ...userData, bio: e.target.value }) }} />
+                                <button className="user-update-btn" onClick={() => { addUserData() }}>Update Details</button>
                             </div>
+                        </div>
+                    </div>
+                    :
+                    null
+            }
+            {
+                chain
+                    ?
+                    <div className="add-chain-main">
+                        <div className="add-chain-box">
+                            <p className="add-chain-message">
+                                Currently our application only supports bittorrent testnet. Please add the BTT chain. If you have already added please switch to BTT.
+                            </p>
+                            <button className="add-chain-btn" onClick={() => { addChain() }}>add chain</button>
                         </div>
                     </div>
                     :
